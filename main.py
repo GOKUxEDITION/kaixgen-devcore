94ab4eeec20a186ba8a3f6db49e2a0c445ed5e75
GOKUxEDITION™ <gokuxedition@gmail.com>
2026-02-12 22:19:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from database.connection import engine
+from database.models import Base
+
+Base.metadata.create_all(bind=engine)
 from fastapi import FastAPI
 from core.ai_engine import AIEngine
 from core.access_control import AccessControl
